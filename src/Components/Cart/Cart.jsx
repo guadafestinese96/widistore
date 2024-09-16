10ff5c0bd9a67fa57363cae5b1f1a7e13f3679aa
guadafestinese <guadalupefestinese@gmail.com>
2024-09-16 19:36:31 -0300

    .

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import {  useContext, useId } from 'react'
+import { useContext, useId } from 'react'
 import cartImg from '../../../public/cart.png'
 import './Cart.css'
 import CartContext from './CartContext';
@@ -7,7 +7,7 @@ import CartItem from './CartItem'
 
 export default function Cart() {
     const cartCheckboxId = useId();
-    const {cart} = useContext(CartContext);
+    const { cart } = useContext(CartContext);
     const quantityProductsCart = cart.length;
 
 
@@ -16,19 +16,24 @@ export default function Cart() {
             <label className='cart-button' htmlFor={cartCheckboxId}>
                 <span className='numberCartSpan'>{quantityProductsCart}</span>
                 <img src={cartImg} alt="cart" className='cartImg' />
-                
+
             </label>
             <input id={cartCheckboxId} type="checkbox" hidden />
-            
+
             <aside className='cart'>
-            <ul key="key">
-                 {cart.map((productoCarrito)=>(
-                    <li key={productoCarrito.id}>
-                    <CartItem item={productoCarrito}/>
-                    </li>
-                 ))}
+                <ul key="key">
+                    {cart.map((productoCarrito) => (
+                        <li key={productoCarrito.id}>
+                            <CartItem item={productoCarrito} />
+                        </li>
+                    ))}
                 </ul>
+                <footer className={cart.length>0 ? 'finalizarCompraFooter' : 'finalizarCompraNone'}>
+                    <button className='finalizarCompraBtn'>Finalizar Compra</button>
+                </footer>
             </aside>
+
         </div>
     )
 }
+
